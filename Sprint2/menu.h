da6fe445de7e6d4b1ddf63dad3e6664276643d89
stevenlt2 <113187124+stevenlt2@users.noreply.github.com>
2024-10-13 23:31:25 -0500

    Create menu.h

---
+++ Sprint2/menu.h
@@ -0,0 +1,13 @@
+#include "boardClass.h"
+
+void menu(game& SOS);
+
+void menuBoardType(game& SOS);
+
+void menuBoardSize(game& SOS);
+
+void menuGameplay(game& SOS);
+
+void MenuMove(Player& Play, game& SOS);
+
+void menuComputerGameplay(game& SOS);
